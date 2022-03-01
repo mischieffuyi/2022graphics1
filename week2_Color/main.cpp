71b28ad745252f14927df3233191107c1539112a
mischieffuyi <79628566+mischieffuyi@users.noreply.github.com>
2022-03-01 12:04:15 +0800

    Add files via upload

---
+++ week2_Color/main.cpp
@@ -0,0 +1,28 @@
+#include <GL/glut.h>
+void display()
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3f(1,1,0);
+
+    glBegin(GL_POLYGON);
+        glColor3f(1,0,0);
+        glVertex2f(-1,-1);
+
+        glColor3f(0,1,0);
+        glVertex2f(+1,-1);
+
+        glColor3f(0,0,1);
+        glVertex2f(0,+1);
+    glEnd();
+
+    glutSwapBuffers();
+}
+int main(int argc, char** argv)
+{
+    glutInit( &argc,argv);
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH );
+    glutCreateWindow("²Ä2¶gµ{¦¡");
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
